15da314c8027ee2ed5c3bd705816c807aa5d29cf
Theo MALLESSON <tmalless@student.42.fr>
2023-05-02 14:05:31 +0200

    wild card done, norme missing

--- srcs/parsing/wild_cards.c
+++ srcs/parsing/wild_cards.c
@@ -6,7 +6,7 @@
 /*   By: tmalless <tmalless@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/04 14:55:58 by tmalless          #+#    #+#             */
-/*   Updated: 2023/04/26 16:28:46 by tmalless         ###   ########.fr       */
+/*   Updated: 2023/05/02 14:01:37 by tmalless         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,10 +43,12 @@ int	looking_for_a_star(char *s)
 
 char	**add_dir(char *s, char **tab)
 {
-	static int	i = 0;
+	int	i;
 
+	i = 0;
+	while (tab[i])
+		i++;
 	tab[i] = s;
-	i++;
 	return (tab);
 }
 
@@ -173,10 +175,14 @@ char	**wild_carder(char *cmd)
 	char			**ans;
 	char			**motif;
 	int				stars;
+	static int				d_number = 0;
 
 	motif = NULL;
+	rep = NULL;
 	motif = create_motif(cmd, motif);
 	rep = opendir(".");
+	if (!rep)
+		printf("ca marche pas zbi \n");
 	ans = ft_calloc(sizeof(char *), 4096);
 	if (cmd[0] != '*' && cmd[ft_strlen(cmd) - 1] != '*')
 		stars = 0;
@@ -189,15 +195,20 @@ char	**wild_carder(char *cmd)
 	lecture = readdir(rep);
 	while (lecture)
 	{
+		long pos = telldir(rep);
+		printf("ici meme : %ld\n", pos);
+		if (d_number > 4095)
+			break ;
 		if (corresponding_dir(lecture->d_name, motif, stars))
 		{
 			add_dir(lecture->d_name, ans);
+			d_number++;
 		}
 		//free(lecture);
 		lecture = readdir(rep);
 	}
-/* 	int v = closedir(rep);
-	printf("Klose: %d\n", v); */
+	//int v = closedir(rep);
+	//printf("Klose %d\n", v);
 	free_tab(motif);
 	return (ans);
 }
@@ -217,25 +228,28 @@ char	**wild_card(char **cmds)
 	int	i;
 	int	j;
 	int	k;
+	int n;
 	char **new_cmd;
 	char **old_cmd;
 	char **dirs;
 
 	i = -1;
 	old_cmd = cmds;
-	while (cmds[++i])
+	while (old_cmd[++i])
 	{
 		j = 0;
 		k = 0;
-		if (ft_strchr(cmds[i], '*'))
+		if (old_cmd[i] && ft_strchr(old_cmd[i], '*'))
 		{
-			dirs = wild_carder(cmds[i]);
-			new_cmd = ft_calloc(tab_size(dirs) + i, sizeof(char *));
+			dirs = wild_carder(old_cmd[i]);
+			new_cmd = ft_calloc(tab_size(dirs) + tab_size(old_cmd), sizeof(char *));
+			printf("dirdir : %s\n", dirs[0]);
 			while (j < i)
 			{
 				new_cmd[j] = old_cmd[j];
 				j++;
 			}
+			printf("le d n'a pas fini de faire parler de lui ! %d\n", k);
 			while (dirs[k])
 			{
 				new_cmd[j] = dirs[k];
@@ -243,13 +257,24 @@ char	**wild_card(char **cmds)
 				k++;
 				j++;
 			}
+			n = i + 1;
+			while (old_cmd[n])
+			{
+				new_cmd[j] = old_cmd[n];
+				printf("new_c : %s\n", new_cmd[j]);
+				j++;
+				n++;
+			}
+			free(old_cmd);
 			old_cmd = new_cmd;
+			//free(dirs);
+			//free(new_cmd);
 		}
 	}
-	if (new_cmd)
-		return (new_cmd);
-	else
-		return (old_cmd);
+	int t = -1;
+	while (old_cmd[++t])
+		printf("c la nouvelle commande ca : %s\n", old_cmd[t]);
+	return (old_cmd);
 }
 
 /* int main(int ac, char **av)
